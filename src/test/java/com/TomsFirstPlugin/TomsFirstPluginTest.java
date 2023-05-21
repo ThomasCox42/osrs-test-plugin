381b38c8582c2041f60c47003303e9c32e276b1b
Thomas Cox <tomtomcox@gmail.com>
2023-05-21 13:24:31 -0600

    Renaming files and adding icon to root.

--- src/test/java/com/TomsFirstPlugin/TomsFirstPluginTest.java
+++ src/test/java/com/TomsFirstPlugin/TomsFirstPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.TomsFirstPlugin;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class TomsFirstPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(TomsFirstPlugin.class);
 		RuneLite.main(args);
 	}
 }
